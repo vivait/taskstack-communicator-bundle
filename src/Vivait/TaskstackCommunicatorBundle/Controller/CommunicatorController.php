eded2794ef574e3e1e8170de23dbb1bacf5ff3e1
Robin Cawser <robincawser@gmail.com>
2015-06-12 10:16:58 +0100

    Add use for Response

--- src/Vivait/TaskstackCommunicatorBundle/Controller/CommunicatorController.php
+++ src/Vivait/TaskstackCommunicatorBundle/Controller/CommunicatorController.php
@@ -5,6 +5,7 @@ namespace Vivait\TaskstackCommunicatorBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Vivait\TaskstackCommunicatorBundle\Model\Issue;
 
