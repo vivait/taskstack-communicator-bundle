29be32873f76406521070dd2ba24ab0dbc2a4595
Lewis Wright <lewis@vivait.co.uk>
2015-07-22 11:36:12 +0100

    Moved from using IDs to classes inside the container, moved javascript to separate file.

---
+++ src/Vivait/TaskstackCommunicatorBundle/Resources/public/js/communicator.js
@@ -0,0 +1,39 @@
+var container            = $('#taskstack-help-button'),
+    errorElement         = $('.help-error', container),
+    loadingElement       = $('.loading', container),
+    helpContainerElement = $('.help-form-container', container);
+
+$('.ajaxForm', container).submit(function (e) {
+    e.preventDefault();
+    var $this;
+
+    $.ajax({
+        type:       $this.attr('method'),
+        url:        $this.attr('action'),
+        data:       $this.serialize(),
+        beforeSend: function () {
+            loadingElement.show();
+            helpContainerElement.hide();
+        }
+    })
+    .always(function () {
+        helpContainerElement.show();
+        loadingElement.hide()
+    })
+    .done(function (data) {
+        errorElement.hide();
+        helpContainerElement.html('<span class="taskstack-help-success"><i class="fa fa-check"></i> ' + data.message + '</span>');
+    })
+    .fail(function (jqXHR) {
+        var responseJSON = jqXHR.responseJSON;
+
+        if (responseJSON.hasOwnProperty('form')) {
+            errorElement.show();
+            helpContainerElement.html(responseJSON.form);
+        }
+        else if (responseJSON.hasOwnProperty('message')) {
+            errorElement.show();
+            errorElement.text(responseJSON.message);
+        }
+    });
+});
